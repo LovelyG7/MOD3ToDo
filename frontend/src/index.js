e43856515a4e0d8cc63d73f6d24c9972727809db
LovelyG7 <nia.gayle@gmail.com>
2023-09-05 05:08:16 -0400

    auth context created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { TodosContextProvider } from './context/ToDoContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
   <React.StrictMode>
+    <AuthContextProvider>
     <TodosContextProvider>
       <App />
     </TodosContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 
 );
